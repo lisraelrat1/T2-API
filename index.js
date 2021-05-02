1d749f7348ad1ed4ffe3907978c38a3bc6fdf015
LIAT ISRAEL <lisrael1@uc.cl>
2021-05-02 09:45:30 -0400

    server edit

--- index.js
+++ index.js
@@ -8,3 +8,12 @@ server.listen(PORT, () => {
     console.log(`Express is working on port ${PORT}`);
 })
 
+server.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
+    res.header(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept'
+    );
+    next();
+  });
--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const routes = require('./routes');
 var cors = require('cors')
@@ -9,22 +8,6 @@ server.use(cors())
 
 server.use('/', routes);
 
-// server.use((req, res, next) => {
-//     res.header('Access-Control-Allow-Origin', '*');
-//     res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
-//     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-//     res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
-//     next();
-// })
-server.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
-    res.header(
-      'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept'
-    );
-    next();
-  });
 
 if (process.env.NODE_ENV === 'production') {
 	server.use(express.static('client/build'));
