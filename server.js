3e88440604e3b3bf68292a738205f9366f22134e
LIAT ISRAEL <lisrael1@uc.cl>
2021-05-01 22:07:45 -0400

    jesus

--- server.js
+++ server.js
@@ -9,4 +9,12 @@ server.use(cors())
 
 server.use('/', routes);
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+})
+
 module.exports = server;
