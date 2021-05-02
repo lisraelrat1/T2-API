9e4b018471a09481da9eee03c5e8b8e532dc07bc
LIAT ISRAEL <lisrael1@uc.cl>
2021-05-02 09:32:20 -0400

    port

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const server = require('./server.js');
 const PORT = process.env.PORT || 3300;
 
 server.listen(PORT, () => {
-    const port = server.address().port;
-    console.log(`Express is working on port ${port}`);
+    console.log(`Express is working on port ${PORT}`);
 })
 
